cd68bee56b17780995dd5c7575a98af8362b0c18
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-05-06 10:45:06 +0700

    Next Level
    Added a function to load the next level in the uiManager

--- Assets/Scripts/Core/NextLevel.cs
+++ Assets/Scripts/Core/NextLevel.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour
 {
+    private UIManager uiManager;
+
+    private void Awake()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            var index = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(index + 1);
+            Time.timeScale = 0f;
+            uiManager.Victory();
         }
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,18 +4,26 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverScreen;
-    [SerializeField] private GameObject VictoryScreen;
+    [SerializeField] private GameObject victoryScreen;
     [SerializeField] private AudioClip gameOverSound;
+    [SerializeField] private AudioClip victorySound;
 
     private void Awake()
     {
-        gameOverScreen.SetActive(false);
+        //gameOverScreen.SetActive(false);
+        //victoryScreen.SetActive(false);
     }
 
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
         SoundManager.instance.PlaySound(gameOverSound);
+    }
+
+    public void Victory()
+    {
+        victoryScreen.SetActive(true);
+        SoundManager.instance.PlaySound(victorySound);
     }
 
     public void Checkpoint()
@@ -31,5 +39,12 @@ public class UIManager : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
+    {
+        Time.timeScale = 1f;
+        var index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
     }
 }
